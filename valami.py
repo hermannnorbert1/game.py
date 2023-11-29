484ef8817d4ad83d2dd954605f0dc976bde194bd
hermannnorbert1 <90178406+hermannnorbert1@users.noreply.github.com>
2023-11-29 13:04:04 +0100

    Create valami.py

---
+++ valami.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# give a title to our app
+st.title('Welcome to BMI Calculator')
+
+# TAKE WEIGHT INPUT in kgs
+weight = st.number_input("Enter your weight (in kgs)")
+
+# TAKE HEIGHT INPUT
+# radio button to choose height format
+status = st.radio('Select your height format: ',
+				('cms', 'meters', 'feet'))
